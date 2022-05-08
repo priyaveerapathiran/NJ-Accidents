2f9284d61f195ec8da68315a4bfa2dd6b6f7089d
priyaveerapathiran <49200181+priyaveerapathiran@users.noreply.github.com>
2022-05-07 23:49:38 -0400

    Data Cleaning
    My first commit

---
+++ acci/ui.R
@@ -0,0 +1,26 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+accidents <- read.
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+          titlePanel("NJ Accidents"),
+          sidebarLayout(
+            sidebarPanel(),
+            mainPanel()
+          )
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            
+        )
+    )
+))
